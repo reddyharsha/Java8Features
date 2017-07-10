f79cd4e36e774c96644234dac869d48bd5b82a0b
hsubbagari <harsha.sub@gmail.com>
2017-07-10 10:47:32 -0400

    Commit join first type

--- Java8Features/src/StringFeatures/StringJoin.java
+++ Java8Features/src/StringFeatures/StringJoin.java
@@ -2,4 +2,12 @@ package StringFeatures;
 
 public class StringJoin {
 
+	public static void main(String args[]) {
+
+		String firstType = String.join(" and ", "PGeorge", "James Harden", "Lebron James");
+		System.out.println(firstType);
+
+		//result
+		//PGeorge and James Harden and Lebron James.
+	}
 }
