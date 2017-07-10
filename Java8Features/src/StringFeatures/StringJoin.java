f007887ba2b785cf7b8c0aab0b4f60dfa2a80d76
hsubbagari <harsha.sub@gmail.com>
2017-07-10 10:52:27 -0400

    Second join method

--- Java8Features/src/StringFeatures/StringJoin.java
+++ Java8Features/src/StringFeatures/StringJoin.java
@@ -4,10 +4,17 @@ public class StringJoin {
 
 	public static void main(String args[]) {
 
+		//First Type
 		String firstType = String.join(" and ", "PGeorge", "James Harden", "Lebron James");
 		System.out.println(firstType);
-
 		//result
 		//PGeorge and James Harden and Lebron James.
+		
+		//Second Type
+		String[] secondType = {"California", "Indiana", "Montana"};
+		System.out.println(String.join(" and ", secondType));
+		//result
+		//California and Indiana and Montana
+
 	}
 }
